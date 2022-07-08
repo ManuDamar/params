985e23ece8109cddeb1bbca3ddd3238e1eed6fb0
Emmanuel Díaz <57876345+ManuDamar@users.noreply.github.com>
2022-07-08 13:46:06 -0500

    Update startstop_unix.d

--- startstop_unix.d
+++ startstop_unix.d
@@ -14,16 +14,16 @@
 # the issue.
 
 
- nRing            		5
+ nRing            	    5
  Ring   STATUS 		    1024
  Ring   WAVE 		    1024
  Ring   TRIG 		    1024
  Ring   SCN 		    1024
- Ring   OSVM_RING      1024
+ Ring   OSVM_RING           1024
 #
- MyModuleId    MOD_STARTSTOP  # Module Id for this program
- HeartbeatInt  15             # Heartbeat interval in seconds
- MyClassName   OTHER             # For this program
+ MyModuleId     MOD_STARTSTOP  # Module Id for this program
+ HeartbeatInt   15             # Heartbeat interval in seconds
+ MyClassName    OTHER             # For this program
  MyPriority     0             # For this program
  LogFile        1             # 1=write a log file to disk, 0=dont
  KillDelay      30            # seconds to wait before killing modules on
@@ -31,8 +31,8 @@
  HardKillDelay  5             # number of seconds to wait on hard shutdown
                               #  for a child to respond to KILL signal
                               #  If missing or 0, no KILL signal will be sent
-#  maxStatusLineLen   80     # Uncomment to specify length of lines in status
-statmgrDelay		5     	# Uncomment to specify the number of seconds
+#  maxStatusLineLen   80      # Uncomment to specify length of lines in status
+statmgrDelay	5      	      # Uncomment to specify the number of seconds
 							# to wait after starting statmgr 
 							# default is 1 second
 
